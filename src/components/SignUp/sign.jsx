9d2fa7006c4f75c2871a49d15fc018975dfde416
Mashrur112 <mashrur950@gmail.com>
2024-05-30 21:30:43 +0600

    sign up page modified

--- src/components/SignUp/sign.jsx
+++ src/components/SignUp/sign.jsx
@@ -1,53 +1,72 @@
-import React from 'react'
-import './sign.css'
+import React from "react";
+import "./sign.css";
 export const Sign = () => {
   return (
     <div>
-     
-    <div className="gap"></div>
-    <div class="wrapper">
-      <form action="#">
-        <div class="login">
-          <h1>SignUp</h1>
-        </div>
-        <div class="input1">
+      <div className="gap"></div>
+      <div class="wrapper_sign">
+        <form action="#">
+          <div class="login">
+            <h1>SignUp</h1>
+          </div>
+          <div class="input1">
+            <div>
+              <ion-icon class="icon" name="mail"></ion-icon>
+              <input
+                type="Email"
+                placeholder="Name"
+                class="email"
+                id="lbl4"
+                required
+              />
+            </div>
+          </div>
+          <div class="input1">
           <div>
             <ion-icon class="icon" name="mail"></ion-icon>
             <input
               type="Email"
-              placeholder="Username"
+              placeholder="Phone"
               class="email"
               id="lbl4"
               required
             />
           </div>
         </div>
-        <div class="input1">
-          <div>
-            <ion-icon class="icon" name="lock-closed"></ion-icon>
-            <input
-              type="password"
-              placeholder="Password"
-              class="pass"
-              id="lbl2"
-            />
+          <div class="input1">
+            <div>
+              <ion-icon class="icon" name="mail"></ion-icon>
+              <input
+                type="Email"
+                placeholder="Email"
+                class="email"
+                id="lbl4"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div class="remember">
-          <input type="checkbox" />
-          <label class="rmmbr">Remember me</label>
-          <a href="#">Forgot Password?</a>
-        </div>
-        <div class="btn">
-          <button type="submit">Login</button>
-        </div>
-        <div className="signup">
-          <h2>
-            Do you hava an account? <a href="/SignUp">Sign up</a>
-          </h2>
-        </div>
-      </form>
+          <div class="input1">
+            <div>
+              <ion-icon class="icon" name="lock-closed"></ion-icon>
+              <input
+                type="password"
+                placeholder="Password"
+                class="pass"
+                id="lbl2"
+              />
+            </div>
+          </div>
+         
+          <div class="btn">
+            <button type="submit">SignUp</button>
+          </div>
+          <div className="signup">
+            <h2>
+              Already have an account? <a href="/Login">Login</a>
+            </h2>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
