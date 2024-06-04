e5aa647236a9d9405f71b069b064e131f7ba2deb
nabihatahsin101 <124432334+nabihatahsin101@users.noreply.github.com>
2024-06-04 23:06:32 +0600

    Update About1.jsx

--- src/components/About/About1.jsx
+++ src/components/About/About1.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
+import "./About.css";
 
 export const About1 = () => {
   return (
     <div>
-      <h1><br></br>About Enchant Tech</h1>
-      <p><br></br>Since its inception on 1st September 2015, Enchant Tech has rapidly 
+      <h1 className="firsthead"><br></br>About Enchant Tech</h1>
+      <p className="p1"><br></br>Since its inception on 1st September 2015, Enchant Tech has rapidly 
         evolved into one of the most trusted names in technology retail throughout our nation. From humble beginnings, 
         our dedication to customer satisfaction and innovation has seen us grow into a leading player in the e-commerce landscape,
          earning the trust and loyalty of countless customers across the country.</p>
-         <h2><br></br>Our Main Goal And Aim</h2>
-         <p><br></br>We are Star Tech, and we are here to help you with all your technology needs.
+         <h2 className="secondhead"><br></br>Our Main Goal And Aim</h2>
+         <p className="p2"><br></br>We are Star Tech, and we are here to help you with all your technology needs.
           We aim to provide all the requirements of our customers and help them satisfy their needs, 
           wants, and desires.  Our complete focus is on the customers. We keep tabs
             and records on what our customers want, and we try our best to bring that to them. 
              They do not have to travel long distances to get their desired product.</p>
-             <h3><br></br>Top Selling Brands</h3>
-         <p><br></br>At Enchant Tech, we focus on top-selling brands like Antec, Asrock, 
+             <h3 className="thirdhead"><br></br>Top Selling Brands</h3>
+         <p className="p3"><br></br>At Enchant Tech, we focus on top-selling brands like Antec, Asrock, 
          Razer, Lian Li, and more, ensuring you get the latest and best products.
           We pride ourselves on excellent after-sales service, swiftly resolving any issues 
           to return your products to pristine condition. Our commitment to quality means you can 
           shop confidently, knowing you're getting top-tier products at the best prices in Bangladesh.
            Your satisfaction with our products and pricing is our top priority.</p>
-           <h4><br></br>Customer-Centric Approach</h4>
-           <p><br></br>Our approach revolves entirely around you - our valued customers.
+           <h4 className="fourthhead"><br></br>Customer-Centric Approach</h4>
+           <p className="p4"><br></br>Our approach revolves entirely around you - our valued customers.
             We listen attentively to your feedback, preferences, and suggestions, and we 
             strive tirelessly to bring your visions to life. 
            Your satisfaction fuels our passion for excellence.<br></br></p>
