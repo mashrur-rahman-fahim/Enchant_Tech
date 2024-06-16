5413939a4483bd9b293f30e1276070abff46e1c4
Mashrur112 <mashrur950@gmail.com>
2024-06-16 19:32:45 +0600

    finally now data is showing of cart from data json

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,26 +1,37 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export const Cart = () => {
-  
-  
-  
-  return (
-    
-    <div>
-        {
-          cartData.map((item)=>{
-         return(
-          <>
-          <button >add</button>
-          {item.name}
-          </>
-         )
+    const [cartData, setCartData] = useState([]);
 
+    useEffect(()=>{
+        fetch('http://localhost:4000/api/cart')
+        .then(response=>response.json())
+        .then(data=>setCartData(data));
+       
 
-          })
+    },[])
 
-        }
-    </div>
-  )
-}
+    return (
+        <div>
+            <ul className="Cart_headers">
+                <li>IMAGE</li>
+                <li>NAME</li>
+                <li>COUNT</li>
+                <li>PRICE</li>
+                <li>REMOVE</li>
+            </ul>
+            {cartData.map((item, index) => (
+                <div className="cart_list" key={index}>
+                    <ul className="data_list">
+                        <li><img src={`../assets/PC/a.jpg`} alt={item.title} /></li>
+                        <li>{item.title}</li>
+                        <li>{item.count || 1}</li> {/* Assuming there's a count field or defaulting to 1 */}
+                        <li>{item.price}</li>
+                        <button className="remove">Remove</button>
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
+};
