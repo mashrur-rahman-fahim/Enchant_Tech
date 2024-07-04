bf2705153246057f07633ce972735724b70316d0
Mashrur112 <mashrur950@gmail.com>
2024-07-05 01:17:45 +0600

    cart data are now in mongoooooooooooooooo, add and delete, delete is now by reducing count of product

---
+++ backend/models/cart.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+
+
+
+const productSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  
+  },
+  count:{
+    type:Number,
+    required:true,
+    unique:true,
+  },
+  img: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  cat: {
+    type: String,
+    required: true
+  },
+  brand: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default:Date.now,
+  }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
