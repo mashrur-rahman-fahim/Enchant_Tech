2bb013cdcddf1a3e9d4cfa125809d8bf4152dd78
Mashrur112 <mashrur950@gmail.com>
2024-07-04 22:56:24 +0600

    backend mongo connect and make it hidden by env

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ import express from 'express';
 import fs from 'fs';
 import cors from "cors"
 import { log } from './middlewares/logger.js';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 
 
-const port = 4000;
+
+const port = process.env.port;
 const app = express();
+mongoose.connect(process.env.mongo_url).then(console.log("Connected to MongoDB")).catch(err => console.log(err));
+
 app.use(express.json());
 app.use(cors());
 app.use(log);
@@ -28,7 +33,7 @@ app.get('/api/cart', (req, res) => {
 
 app.post('/api/cart', (req, res) => {
     const { id, img, title, description, price, cat, brand, date } = req.body;
-  const newProduct = { id, img, title, description, price, cat, brand,date};
+    const newProduct = { id, img, title, description, price, cat, brand, date };
     fs.readFile('./data.json', 'utf-8', (err, data) => {
         if (err) {
             res.status(500).json({ message: "Error reading file" });
@@ -36,8 +41,8 @@ app.post('/api/cart', (req, res) => {
         }
 
         const users = JSON.parse(data);
-       
-       const newData = JSON.stringify([...users,newProduct]);
+
+        const newData = JSON.stringify([...users, newProduct]);
 
         fs.writeFile('./data.json', newData, (err) => {
             if (err) {
@@ -48,56 +53,60 @@ app.post('/api/cart', (req, res) => {
         });
     });
 });
-app.delete('/api/cart/:id',(req,res)=>{
-    const {id}=req.params;
-    fs.readFile('./data.json','utf-8',(err,data)=>{
-        if(err)
-            {
-                res.status(500).json({message:"error reading file"})
-                return ;
+app.delete('/api/cart/:id', (req, res) => {
+    const { id } = req.params;
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).json({ message: "error reading file" })
+            return;
+        }
+        const user = JSON.parse(data);
+        console.log(user);
+        const user_find = user.find((usr) => usr.id === (id - '0'));
+        if (!user_find) {
+            res.status(404).json({ message: "user not found" })
+            return;
+        }
+        res.status(201).json({ message: "found" });
+        const newUser = user.filter((usr) => usr.id != (id - '0'))
+        const newData = JSON.stringify(newUser);
+        fs.writeFile('./data.json', newData, (err) => {
+            if (err) {
+                res.status(500).json({ message: "error writing file" })
+                return;
             }
-            const user=JSON.parse(data);
-            console.log(user);
-            const user_find=user.find((usr)=>usr.id===(id-'0'));
-            if(!user_find){
-                res.status(404).json({message:"user not found"})
-                return;}
-                res.status(201).json({message:"found"});
-                const newUser=user.filter((usr)=>usr.id!=(id-'0'))
-                const newData=JSON.stringify(newUser);
-                fs.writeFile('./data.json',newData,(err)=>{
-                    if(err){
-                        res.status(500).json({message:"error writing file"})
-                        return;}
-                        res.status(201).json({message:"deleted successfully"});
-                })
-           
-            
-            
+            res.status(201).json({ message: "deleted successfully" });
+        })
+
+
+
 
 
     })
-    
+
 })
-app.get('/login',(req,res)=>{
-    fs.readFile('./login_data.json','utf-8',(err,data)=>{
-        if(err){
-            return;}
-            res.status(201).send(JSON.parse(data));
+app.get('/login', (req, res) => {
+    fs.readFile('./login_data.json', 'utf-8', (err, data) => {
+        if (err) {
+            return;
+        }
+        res.status(201).send(JSON.parse(data));
     })
 })
-app.post('/login',(req,res)=>{
-    const{name,email,password}=req.body;
-    fs.readFile('./login_data.json','utf-8',(err,data)=>{
-        if(err){
-            return;}
-            const User=JSON.parse(data);
-            const newUser=JSON.stringify([...User,{name,email,password}])
-            fs.writeFile('./login_data.json',newUser,(err)=>{
-                if(err){
-                    return ;}
-                    res.status(201).json({message:"user created successfully"});
-            })
+app.post('/login', (req, res) => {
+    const { name, email, password } = req.body;
+    fs.readFile('./login_data.json', 'utf-8', (err, data) => {
+        if (err) {
+            return;
+        }
+        const User = JSON.parse(data);
+        const newUser = JSON.stringify([...User, { name, email, password }])
+        fs.writeFile('./login_data.json', newUser, (err) => {
+            if (err) {
+                return;
+            }
+            res.status(201).json({ message: "user created successfully" });
+        })
 
 
     })
