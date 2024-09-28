3e46c9323f8efa1d50d7fab1300d7ede8c149c55
mashrur-rahman-fahim <mashrur950@gmail.com>
2024-09-28 18:34:00 +0600

    individual product details

--- frontend/src/components/Product/AllProduct.jsx
+++ frontend/src/components/Product/AllProduct.jsx
@@ -14,7 +14,7 @@ export const AllProduct = () => {
     const fetchProduct = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:4000/products/${id}`
+          `http://localhost:4000/productObject/${id}`
         );
         setProduct(response.data);
         setLoading(false);
